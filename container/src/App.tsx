84d554a77a473943e6bcaa88a67547daae6aed48
miles <miles.yan114@gmail.com>
2021-08-18 16:33:42 +0800

    use router

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,4 +1,3 @@
-import React, {useEffect} from 'react';
 import Index from './page';
 
 export default function App() {
--- container/src/page/index.tsx
+++ container/src/page/index.tsx
@@ -1,10 +1,21 @@
 import Layout from 'basicComponents/components/Layout';
 import OrderApp from 'orderApp/page/orderApp'
+import React from 'react';
+import { Switch, Route, Redirect, HashRouter} from "react-router-dom";
 
 export default function Home() {
   return (
     <Layout>
-      <OrderApp/>
+      <HashRouter>
+        <React.Suspense fallback={"Loading"}>
+          <Switch>
+            <Route path="/order">
+              <OrderApp />
+            </Route>
+            <Redirect to='/order' />
+          </Switch>
+        </React.Suspense>
+      </HashRouter>
     </Layout>
   )
 }
