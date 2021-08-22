c38d7645a646a4bc8097588b55e9593129443d78
miles <miles.yan114@gmail.com>
2021-08-22 15:19:52 +0800

    add hook ut

--- orderApp/src/hooks/__tests__/hook.test.ts
+++ orderApp/src/hooks/__tests__/hook.test.ts
@@ -1,7 +1,8 @@
 import useUpdateEffect from '../useUpdateEffect';
 import { renderHook } from '@testing-library/react-hooks'
+import useTitle from '../useTitle';
 
-it('should run effect on update', () => {
+test('useUpdateEffect should run effect on update', () => {
   const effect = jest.fn();
 
   const { rerender } = renderHook(() => useUpdateEffect(effect));
@@ -11,7 +12,7 @@ it('should run effect on update', () => {
   expect(effect).toHaveBeenCalledTimes(1);
 });
 
-it('should run cleanup on unmount', () => {
+test('useUpdateEffect should run cleanup on unmount', () => {
   const cleanup = jest.fn();
   const effect = jest.fn().mockReturnValue(cleanup);
   const hook = renderHook(() => useUpdateEffect(effect));
@@ -21,3 +22,16 @@ it('should run cleanup on unmount', () => {
 
   expect(cleanup).toHaveBeenCalledTimes(1);
 });
+
+test('useTitle should update title', () => {
+  document.title = 'prev title'
+  const hook = renderHook((title: string) => useTitle(title), {
+    initialProps: 'title 1'
+  })
+  expect(document.title).toBe('title 1');
+  hook.rerender('title 2')
+  expect(document.title).toBe('title 2');
+  hook.unmount();
+  expect(document.title).toBe('prev title');
+
+})
